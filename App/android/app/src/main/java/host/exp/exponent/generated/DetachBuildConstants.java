c4382d6f89f22cc0540570b5fd3a284b04a59930
Rikard <rikerik@student.chalmers.se>
2018-04-22 17:06:57 +0200

    Testmiljö
    Testmiljö för Rikards Dator

--- App/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ App/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-    public static final String DEVELOPMENT_URL = "exp432e0186c47243658eeef37602b6c1c4://7k-3vy.claralouisa.app.exp.direct:80";
+    public static final String DEVELOPMENT_URL = "exp432e0186c47243658eeef37602b6c1c4://dz-st5.claralouisa.app.exp.direct:80";
 
 }
