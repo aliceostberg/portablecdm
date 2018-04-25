a1c6704429de2b4411b5ce76d5257cc8fec0ed01
ahlsell <levinahlsell@icloud.com>
2018-04-25 13:24:22 +0200

    revert to commit a935a981ff25c3a3f84b14844362c253cec175af

--- App/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ App/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,10 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-<<<<<<< HEAD
-    public static final String DEVELOPMENT_URL = "exp432e0186c47243658eeef37602b6c1c4://10.0.118.15:19000";
-=======
     public static final String DEVELOPMENT_URL = "exp432e0186c47243658eeef37602b6c1c4://ge-yxc.claralouisa.app.exp.direct:80";
->>>>>>> a935a981ff25c3a3f84b14844362c253cec175af
 
 }
